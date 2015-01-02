4e98f90f6047834f12934a3ac7a8505a60060ed0
daniel-llach <daniel.llach@gmail.com>
2015-01-02 09:12:00 -0300

    [FIX] localhost por ip

--- ch8/rest-express/test/index.js
+++ ch8/rest-express/test/index.js
@@ -5,7 +5,7 @@ describe('express rest api server', function(){
   var id
 
   it('post object', function(done){
-    superagent.post('http://localhost:3000/collections/test')
+    superagent.post('http://caldearte.cl:3000/collections/test')
       .send({ name: 'John',
         email: 'john@rpjs.co'
       })
@@ -20,7 +20,7 @@ describe('express rest api server', function(){
   })
 
   it('retrieves an object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+    superagent.get('http://caldearte.cl:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -32,7 +32,7 @@ describe('express rest api server', function(){
   })
 
   it('retrieves a collection', function(done){
-    superagent.get('http://localhost:3000/collections/test')
+    superagent.get('http://caldearte.cl:3000/collections/test')
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -43,7 +43,7 @@ describe('express rest api server', function(){
   })
 
   it('updates an object', function(done){
-    superagent.put('http://localhost:3000/collections/test/'+id)
+    superagent.put('http://caldearte.cl:3000/collections/test/'+id)
       .send({name: 'Peter',
         email: 'peter@yahoo.com'})
       .end(function(e, res){
@@ -56,7 +56,7 @@ describe('express rest api server', function(){
   })
 
   it('checks an updated object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+    superagent.get('http://caldearte.cl:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -68,7 +68,7 @@ describe('express rest api server', function(){
       })
   })
   it('removes an object', function(done){
-    superagent.del('http://localhost:3000/collections/test/'+id)
+    superagent.del('http://caldearte.cl:3000/collections/test/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
